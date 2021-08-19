6e7842040b75fc16f33f561f76f0ea9eaf0062db
IAmMasterCraft <bolu.akinsefunmi@gmail.com>
2021-08-19 20:54:53 +0100

    work done

---
+++ app/src/main/java/com/mastercraft/myapplication/MainActivity.java
@@ -0,0 +1,39 @@
+package com.mastercraft.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextInputEditText input = findViewById(R.id.msg);
+        TextView output = findViewById(R.id.output);
+
+        input.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                output.setText(getString(R.string.label, s.toString()));
+            }
+        });
+    }
+}
